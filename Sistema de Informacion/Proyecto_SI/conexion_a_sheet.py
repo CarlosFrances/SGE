9acfdcb69924e9488a8c2acd385f094af8c5f560
CarlosFrances <113194702+CarlosFrances@users.noreply.github.com>
2023-01-23 14:44:58 +0100

    Update conexion_a_sheet.py

--- Sistema de Informacion/Proyecto_SI/conexion_a_sheet.py
+++ Sistema de Informacion/Proyecto_SI/conexion_a_sheet.py
@@ -1,10 +1,19 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("claves.json",scope)
-client = gspread.authorize(creds)
-sheet = client.open("recepcion_formulario").sheet1
+scope = ["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"] # conexion a las apis necesarias
+creds = ServiceAccountCredentials.from_json_keyfile_name("claves.json",scope) # recoger la clave JSON del archivo claves.json
+client = gspread.authorize(creds) #autorizar con la clave JSON 
+sheet = client.open("recepcion_formulario").sheet1 # sheet1 elige la primera hoja de excel
 
 # acceso a datos
-print(sheet.get_all_records())
+#records=sheet.get_all_records() saca toda la hoja excel en formato [{}]
+#records = sheet.col_values(1) saca los valores de la columna 1.  print(records[1:]) saca solo los valores
+records = sheet.col_values(3)
+records = records[1:]
+#records = [int(records) for records in records] transforma el array de strings a enteros
+def mediaNotas(arr):
+    return sum(arr) / len(arr)
+
+
+print(mediaNotas(records))
